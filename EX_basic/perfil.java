16af09425b5861ff512bade32e95a05bb1bbc945
hgolfetto <golfettohenrique@gmail.com>
2025-03-19 11:18:03 -0300

    test - inserting into the folder

--- EX_basic/perfil.java
+++ EX_basic/perfil.java
@@ -1,7 +1,7 @@
+package EX_basic;
 public class perfil {
     public static void main(String[] args) {
         System.out.println("Olá, Henrique");
         System.out.println("Tudo bem?");
-
     }
 }
