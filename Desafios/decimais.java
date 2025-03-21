d7416f6d0244320712486933715cba8f4c4376cf
hgolfetto <golfettohenrique@gmail.com>
2025-03-21 11:18:58 -0300

    feat: media

---
+++ Desafios/decimais.java
@@ -0,0 +1,12 @@
+public class decimais {
+    public static void main(String[] args) {
+        double nota1 = 8.5;
+        double nota2 = 6.9;
+
+        double media = (nota1 + nota2) / 2;
+
+        int resultado = (int) (media);
+
+        System.out.println(resultado);
+    }
+}
