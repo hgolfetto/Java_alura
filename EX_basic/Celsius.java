14fddf7fd31f838e66bcfbb262c83d7e587b9426
hgolfetto <golfettohenrique@gmail.com>
2025-03-21 11:13:57 -0300

    fix: casting

--- EX_basic/Celsius.java
+++ EX_basic/Celsius.java
@@ -2,7 +2,9 @@ public class Celsius {
     public static void main(String[] args) {
         double celsius = 45;
 
-        double farenheit = (celsius * 1.8) + 32;
+        double calculo = (celsius * 1.8) + 32;
+
+        int farenheit = (int) (calculo);
 
         System.out.println(farenheit);
     }
