5bbe9324db2bbb6223fc823407bc9b4f59cba6f4
hgolfetto <golfettohenrique@gmail.com>
2025-03-21 11:28:15 -0300

    feat: calculation and contact exercise

---
+++ Desafios/produto.java
@@ -0,0 +1,11 @@
+public class produto {
+    public static void main(String[] args) {
+        
+        double precoProduto = 50.00;
+        int quantidade = 5;
+
+        double calculo = precoProduto * quantidade;
+
+        System.out.println("O valor total é de: " + calculo);
+    }
+}
