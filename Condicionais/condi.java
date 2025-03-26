dc6c4cc7c09e895ed06437df7fb6420ca7176273
hgolfetto <golfettohenrique@gmail.com>
2025-03-26 09:36:58 -0300

    feat: condicional exercise

---
+++ Condicionais/condi.java
@@ -0,0 +1,22 @@
+public class condi {
+    public static void main(String[] args) {
+        
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
+
+        if (anoDeLancamento > 2022){
+            System.out.println("Lançamento que os clientes estão curtindo");
+        } else {
+            System.out.println("Filme retrô");
+        }
+
+        if (incluidoNoPlano == true || tipoPlano.equals("plus")){
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Deve pagar a locação");
+        }
+
+    }
+}
